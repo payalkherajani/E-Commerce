06e1f43ff7a31e2ec8d96ff645e7745d169de9ba
payalkherajani <payal.kherajani19@gmail.com>
2021-03-23 22:31:42 +0530

    frontend:added-dummy-data-&-miragejs-server

---
+++ frontend/src/api/server.js
@@ -0,0 +1,36 @@
+import { createServer, Model, Response } from 'miragejs';
+import productsData from './products';
+
+export function makeServer() {
+
+    let server = createServer({
+
+        //Send data in JSON Format
+        serializers: {
+            application: RestSerializer
+        },
+
+        //Models
+        models: {
+            products: Model,
+        },
+
+        //Routes
+        routes() {
+            this.namespace = "api"
+
+            this.get("/products", (schema, request) => {
+                return schema.products.all()
+            })
+        },
+
+        //Dummy Data
+        seeds(server) {
+            productsData.map((product) => server.create("products", product))
+        },
+
+    })
+
+    return server;
+
+}
