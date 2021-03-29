d886341afeb478b84de99e9aa8690cdd23cdd179
payalkherajani <payal.kherajani19@gmail.com>
2021-03-29 16:46:42 +0530

    frontend:wishlist-design-done

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Message = ({ variant, children }) => {
     return (
-        <>
+        <div className="message m-1">
             {
-                variant === 'danger' ? (<div class="alert alert-danger"> { children} </div>) :
-                    variant === 'success' ? (<div class="alert alert-success">{children}</div>) :
-                        (<div class="alert alert-info">{children}</div>)
+                variant === 'danger' ? (<div className="alert alert-danger"> { children} </div>) :
+                    variant === 'success' ? (<div className="alert alert-success">{children}</div>) :
+                        (<div className="alert alert-info">{children}</div>)
             }
-        </>
+        </div>
     )
 }
 
--- frontend/src/screens/Wishlist/index.js
+++ frontend/src/screens/Wishlist/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import useCustomContext from '../../customHooks/Hook';
 import { REMOVE_FROM_WISHLIST } from '../../constants/WishListConstants';
+import { Message, Rating } from '../../components';
+import { Link } from 'react-router-dom';
+import styles from './wishlist.module.css';
 
 const WishList = () => {
     const { state: { wishlist }, dispatch } = useCustomContext();
@@ -10,17 +13,37 @@ const WishList = () => {
     }
 
     return (
-        <div>
+        <div className={styles.wishlist_container}>
+            <Link to='/products' className="p-half"><button className="btn btn-primary m-b-half">Go Back</button></Link>
+
             {
-                wishlist && wishlist.map((wish) => (
-                    <ul key={wish.id}>
-                        <li>{wish.id}</li>
-                        <li>{wish.name}</li>
-                        <li>{wish.description}</li>
-                        <button onClick={() => removefromWishList(wish.id)}>Remove From WishList </button>
-                    </ul>
-                ))
-            }
+                wishlist.length === 0 ?
+                    (<Message>Your Wishlist is Empty</Message>)
+                    :
+                    (
+                        <div className={styles.wishlist_card}>
+                            {
+                                wishlist.map(({ id, name, description, image, price, numReviews, rating }) => (
+                                    <div key={id} className={styles.single_wishlist_card}>
+
+                                        <div className={styles.wishlist_image_container}>
+                                            <img src={image} className="wishlist_image" className={styles.wishlist_image} />
+                                        </div>
+
+                                        <div className={styles.wishlist_details_container}>
+                                            <ul className="list-group">
+                                                <li className={styles.list_item}><strong>{name}</strong></li>
+                                                <li className={styles.list_item}>{description}</li>
+                                                <li className={styles.list_item}>₹{price}</li>
+                                                <li className={styles.list_item}><Rating value={rating} numReviews={numReviews} /></li>
+                                                <li className={styles.list_item}><button className="btn btn-danger" onClick={() => removefromWishList(id)}>Remove</button></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    )}
         </div>
     )
 }
