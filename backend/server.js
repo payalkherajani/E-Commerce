19f901d2d59a18b4d85bb15e17705f0fcc250644
payalkherajani <payal.kherajani19@gmail.com>
2021-04-19 10:34:38 +0530

    backend:mongo-db-connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
 const app = express();
+
+connectDB()
+
 const PORT = 5000 || process.env.PORT;
 
 
