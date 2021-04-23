af6545c2d383b08ca830b6b2f9ea51b82ef1bdfd
payalkherajani <payal.kherajani19@gmail.com>
2021-04-23 15:14:49 +0530

    backend:product-initial-setup

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,6 +1,45 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const productSchema = new Schema({
+const productSchema = new Schema(
+    {
+        name: {
+            type: String
+        },
 
-})
+        image: {
+            type: String
+        },
+
+        description: {
+            type: String
+        },
+
+        price: {
+            type: Number
+        },
+
+        countInStock: {
+            type: Number
+        },
+
+        rating: {
+            type: Number
+        },
+
+        numReviews: {
+            type: Number
+        },
+
+        qty: {
+            type: Number
+        }
+    },
+
+    {
+        timestamps: true
+    }
+)
+
+const Product = mongoose.model('product', productSchema);
+module.exports = Product;
