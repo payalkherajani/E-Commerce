f4e987b91686a82df1502c5ae9e74e37e441cb59
payalkherajani <payal.kherajani19@gmail.com>
2021-04-29 12:13:26 +0530

    frontend:login-register-setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Products, SingleProduct, WishList, Landing, Cart } from './screens';
-import { Navbar } from './components'
+import { Products, SingleProduct, WishList, Landing, Cart, Register, Login } from './screens';
+import { Navbar } from './components';
+import PrivateRoute from './routes/Privateroute';
 
 const App = () => {
   return (
@@ -12,10 +13,12 @@ const App = () => {
           <Fragment>
             <Navbar />
             <Route exact path='/' component={Landing} />
-            <Route exact path='/products' component={Products} />
-            <Route exact path='/product/:id' component={SingleProduct} />
-            <Route exact path='/wishlist' component={WishList} />
-            <Route exact path='/cart' component={Cart} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+            <PrivateRoute exact path='/products' component={Products} />
+            <PrivateRoute exact path='/product/:id' component={SingleProduct} />
+            <PrivateRoute exact path='/wishlist' component={WishList} />
+            <PrivateRoute exact path='/cart' component={Cart} />
           </Fragment>
         </Switch>
       </Router>
--- frontend/src/screens/index.js
+++ frontend/src/screens/index.js
@@ -3,3 +3,5 @@ export { default as Cart } from './Cart';
 export { default as Landing } from './Landing';
 export { default as SingleProduct } from './SingleProduct';
 export { default as WishList } from './Wishlist';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
