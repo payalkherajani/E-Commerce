0f77d7ab50b179f46bedf2d69ae947b049e8636f
payalkherajani <payal.kherajani19@gmail.com>
2021-04-29 15:51:19 +0530

    frontend:added-logout-feature

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -15,29 +15,58 @@ const Navbar = () => {
             <button className="hamburger-icon" onClick={() => setVisible((visible) => !visible)}>
                 <img src={icon} alt="hamburger-icon" />
             </button>
+            {
+                localStorage.getItem('TOKEN') ? (
+                    <ul className={visible === true ? ("nav-list") : (" nav-list nav-list-none")}>
 
-            <ul className={visible === true ? ("nav-list") : (" nav-list nav-list-none")}>
+                        <Link to={{ pathname: '/products' }}>
+                            <li className="badge-relative" onClick={() => setVisible((visible) => !visible)}>
+                                Products
+                        </li>
+                        </Link>
 
-                <Link to={{ pathname: '/products' }}>
-                    <li className="badge-relative" onClick={() => setVisible((visible) => !visible)}>
-                        Products
-                    </li>
-                </Link>
+                        <Link to={{ pathname: '/wishlist' }}>
+                            <li className="badge-relative" onClick={() => setVisible((visible) => !visible)}>
+                                WishList
+                            <span className="badge-nav-wishlist">{state.wishlist.length}</span>
+                            </li>
+                        </Link>
+                        <Link to={{ pathname: '/cart' }}>
+                            <li className="badge-relative" onClick={() => setVisible((visible) => !visible)}>
+                                Cart
+                            <span className="badge-nav-cart">{state.cart.length}</span>
+                            </li>
+                        </Link>
+                        <Link to={{ pathname: '/' }}>
+                            <li className="badge-relative" onClick={() => {
+                                setVisible((visible) => !visible)
+                                localStorage.removeItem('TOKEN')
+                            }}>
+                                <i class="fas fa-sign-out-alt"></i>
+                            </li>
+                        </Link>
 
-                <Link to={{ pathname: '/wishlist' }}>
-                    <li className="badge-relative" onClick={() => setVisible((visible) => !visible)}>
-                        WishList
-                        <span className="badge-nav-wishlist">{state.wishlist.length}</span>
-                    </li>
-                </Link>
-                <Link to={{ pathname: '/cart' }}>
-                    <li className="badge-relative" onClick={() => setVisible((visible) => !visible)}>
-                        Cart
-                        <span className="badge-nav-cart">{state.cart.length}</span>
-                    </li>
-                </Link>
-            </ul>
+                    </ul>
+                ) : (
+                    <ul className={visible === true ? ("nav-list") : (" nav-list nav-list-none")}>
+
+                        <Link to={{ pathname: '/login' }}>
+                            <li className="badge-relative" onClick={() => setVisible((visible) => !visible)}>
+                                Login
+                       </li>
+                        </Link>
+
+
+                        <Link to={{ pathname: '/register' }}>
+                            <li className="badge-relative" onClick={() => setVisible((visible) => !visible)}>
+                                Register
+                        </li>
+                        </Link>
+                    </ul>
+                )
+            }
         </nav>
+
     )
 }
 
