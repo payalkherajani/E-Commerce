5addbdd02744e0c57c9df13e9b4118c12cb46112
payalkherajani <payal.kherajani19@gmail.com>
2021-04-29 13:45:52 +0530

    frontend:login-register-template-design-done

--- frontend/src/screens/Login/index.js
+++ frontend/src/screens/Login/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import styles from './login.module.css';
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className={styles.login__container}>
+            <h1>Login</h1>
+            <form>
+                <div className={styles.form_container}>
+                    <input
+                        className={styles.input_login}
+                        placeholder="Email"
+                        type="email"
+                        name="email"
+                    />
+                </div>
+
+                <div className={styles.form_container}>
+                    <input
+                        className={styles.input_login}
+                        placeholder="Password"
+                        type="password"
+                        name="password"
+
+                    />
+                </div>
+
+
+                <button className={`btn btn-primary ${styles.login_button}`}>Login</button>
+            </form>
         </div>
     )
 }
--- frontend/src/screens/Register/index.js
+++ frontend/src/screens/Register/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import styles from './register.module.css';
 
 const Register = () => {
+
     return (
-        <div>
-            Register
+        <div className={styles.login__container}>
+            <h1>Register</h1>
+            <form>
+
+                <div className={styles.form_container}>
+                    <input
+                        className={styles.input_login}
+                        placeholder="Name"
+                        type="text"
+                        name="name"
+                    />
+                </div>
+
+                <div className={styles.form_container}>
+                    <input
+                        className={styles.input_login}
+                        placeholder="Email"
+                        type="email"
+                        name="email"
+                    />
+                </div>
+
+                <div className={styles.form_container}>
+                    <input
+                        className={styles.input_login}
+                        placeholder="Password"
+                        type="password"
+                        name="password"
+
+                    />
+                </div>
+
+                <button className={`btn btn-primary ${styles.login_button}`}>Register</button>
+            </form>
         </div>
     )
 }
